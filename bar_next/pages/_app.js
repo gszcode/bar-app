9714cd0744e2f146ff02170c782c7e4c4be5a8a0
Gabriel <gszcode@gmail.com>
2023-04-10 18:40:01 -0300

    useState para carrito

--- bar_next/pages/_app.js
+++ bar_next/pages/_app.js
@@ -1,6 +1,41 @@
 import '@/styles/normalize.css'
 import '@/styles/globals.css'
+import { useState, useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [carrito, setCarrito] = useState([])
+
+  useEffect(() => {
+    const carritoLocalS = JSON.parse(localStorage.getItem('carrito')) ?? []
+
+    setCarrito(carritoLocalS)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+  }, [carrito])
+
+  const agregarCarrito = (producto) => {
+    if (carrito.some((bebida) => bebida.id === producto.id)) {
+      const carritoActualizado = carrito.map((bebida) => {
+        if (bebida.id === producto.id) {
+          bebida.cantidad = producto.cantidad
+        }
+
+        return bebida
+      })
+
+      setCarrito(carritoActualizado)
+    } else {
+      setCarrito([...carrito, producto])
+    }
+  }
+
+  return (
+    <Component
+      {...pageProps}
+      carrito={carrito}
+      agregarCarrito={agregarCarrito}
+    />
+  )
 }
