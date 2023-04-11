84aa8348e27ee30477133b15013e7391ebdd20a7
Gabriel <gszcode@gmail.com>
2023-04-10 22:37:04 -0300

    productos agregados al carrito renderizados

--- bar_next/pages/carrito.js
+++ bar_next/pages/carrito.js
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from '../styles/Carrito.module.css'
 import Image from 'next/legacy/image'
 
-const Carrito = ({ carrito }) => {
+const Carrito = ({ carrito, actualizarCantidad, eliminarProducto }) => {
   return (
     <Layout pagina="Carrito de Compras">
       <h1 className="heading">Carrito</h1>
@@ -24,17 +24,41 @@ const Carrito = ({ carrito }) => {
                   </div>
                   <div>
                     <p className={styles.nombre}>{producto.nombre}</p>
-                    <p className={styles.cantidad}>
-                      Cantidad: {producto.cantidad}
-                    </p>
+                    <div className={styles.cantidad}>
+                      <p>Cantidad:</p>
+                      <select
+                        value={producto?.cantidad}
+                        className={styles.select}
+                        onChange={(e) =>
+                          actualizarCantidad({
+                            cantidad: e.target.value,
+                            id: producto.id
+                          })
+                        }
+                      >
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                      </select>
+                    </div>
                     <p className={styles.precio}>
-                      <span>{producto.precio}</span>
+                      $<span>{producto.precio}</span>
                     </p>
                     <p className={styles.subtotal}>
                       Subtotal: $
                       <span>{producto.precio * producto.cantidad}</span>
                     </p>
                   </div>
+
+                  <button
+                    type="button"
+                    className={styles.eliminar}
+                    onClick={() => eliminarProducto(producto.id)}
+                  >
+                    X
+                  </button>
                 </div>
               ))}
         </div>
